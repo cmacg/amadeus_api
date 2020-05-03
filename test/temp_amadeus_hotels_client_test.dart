c17c9ae388ec8202bdf8e3ea99a4d33606d53153
Chris McGinnis <chris.e.mcginnis@gmail.com>
2020-05-03 19:49:01 +0200

    finishing up the hotel client.  it now authenticates and executes the search against the test and production API

---
+++ test/temp_amadeus_hotels_client_test.dart
@@ -0,0 +1,21 @@
+import 'package:amadeusapi/amadeus_hotels_client.dart';
+import 'package:amadeusapi/models/hotels/hotel_offers.dart';
+
+final String apiKey = '';
+final String apiSecret = '';
+
+void main() async {
+
+  AmadeusHotelsClient hotelClient = AmadeusHotelsClient(apiKey: apiKey, apiSecret: apiSecret, test: true);
+  String token = await hotelClient.authorize();
+  print('Token: ' + token);
+
+  //https://test.api.amadeus.com/v2/shopping/hotel-offers?cityCode=PAR&adults=1&radius=5
+  // &radiusUnit=KM&paymentPolicy=NONE&includeClosed=false&bestRateOnly=true&view=FULL&sort=PRICE
+
+  List<HotelOffers> offers = await hotelClient.hotelSearch(cityCode: 'PAR', adults: 1, radius: 5, radiusUnit: RadiusUnit.KM,
+      paymentPolicy: PaymentPolicy.NONE, includeClosed: false, bestRateOnly: true, viewType: ViewType.FULL,
+      sortType: SortType.PRICE);
+
+  print('We got offers ' + offers.toString());
+}
